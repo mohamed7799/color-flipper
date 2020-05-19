0806f91633d2ba1d06e1f40ebd02ec5adb3e0768
mohmed <mohmedamged9@gmail.com>
2020-05-19 17:57:12 +0200

    added the hex color functionality and made the app responsive

--- code.js
+++ code.js
@@ -6,6 +6,8 @@ let hex = document.getElementById("hex-js");
 let color_btn = document.getElementById("color-block__btn");
 let color = document.getElementById("color");
 let color_block = document.getElementById('color-block');
+let mode_status = { simple: 1, hex: 0 };
+let dec = document.getElementById("color-bloack__dec");
 
 let simple_colors = [
     '#88d1d3',
@@ -18,20 +20,46 @@ let simple_colors = [
 
 //functions
 
-function getRandomArbitrary(max) {
-    return Math.floor(Math.random() * max);
-}
+let get_random_int = (max) => { return Math.floor(Math.random() * max); }
 
-let get_random = function (arr) {
-    let index = getRandomArbitrary(arr.length - 1);
-    return arr[index];
-}
+let get_random_hex = () => { return '#' + (Math.random() * 0xFFFFFF << 0).toString(16); }
+
+let get_random_color = (arr) => { return arr[get_random_int(arr.length - 1)]; }
 
 
 //event listeners
 
-color_btn.addEventListener('click', () => {
-    let new_color = get_random(simple_colors)
-    color.textContent = new_color;
-    color_block.style.backgroundColor = new_color;
+
+simple.addEventListener('click', () => {
+    mode_status.simple = 1;
+    mode_status.hex = 0;
+    simple.classList.add("active");
+    hex.classList.remove("active");
+    dec.textContent = "you can choose between 6 random colors";
 })
+
+
+hex.addEventListener('click', () => {
+    mode_status.hex = 1;
+    mode_status.simple = 0;
+    hex.classList.add("active");
+    simple.classList.remove("active");
+    dec.textContent = "you can choose between infinte number of random colors";
+})
+
+
+color_btn.addEventListener('click', () => {
+    if (mode_status.simple === 1) {
+        let new_color = get_random_color(simple_colors)
+        color.textContent = new_color;
+        color_block.style.backgroundColor = new_color;
+    }
+    else {
+
+        let new_color = get_random_hex(simple_colors)
+        color.textContent = new_color;
+        color_block.style.backgroundColor = new_color;
+    }
+})
+
+
